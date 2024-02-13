7ddf05a9c264a83f65b6bd6640a731e3c76be283
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-13 12:21:38 +0900

    02_01_

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
+#include"Enemy.h"
 #include <stdio.h>
+#include <Windows.h>
 
-// aaaaaaaaa
+void (Enemy::* Enemy::pPhaseTable[])() = {
+	&Enemy::Approach,//0
+	&Enemy::Shot,	 //1
+	&Enemy::Leave,	 //2
+};
 
+void Enemy::Init() {
+	//pFunc = &Enemy::TestFunc;
+}
 
-int main() {
-	printf("Hello");
-	return 0;
+typedef void(*PFunc)(int*);
+
+void DispResult(int* s) {
+
+}
+void setTimeout(PFunc p, int second) {
+	Sleep(second * 1000);
+	p(&second);
+}
+
+void Enemy::Update() {
+	switch (phase_) {
+
+	case Phase::kApproach:
+		Approach();
+		break;
+
+	case Phase::kShot:
+		Enemy::Shot();
+		break;
+
+	case Phase::kLeave:
+		Enemy::Leave();
+		break;
+	}
+
+	(this->*pPhaseTable[0])();
+	PFunc p;
+	p = DispResult;
+	setTimeout(p, 3);
+	(this->*pPhaseTable[1])();
+	p = DispResult;
+	setTimeout(p, 3);
+	(this->*pPhaseTable[2])();
+}
+
+void Enemy::Approach() {
+
+	printf("Ú‹ß\n");
+}
+
+void Enemy::Leave() {
+
+	printf("—£’E\n");
+}
+
+void Enemy::Shot() {
+
+	printf("ËŒ‚\n");
 }
