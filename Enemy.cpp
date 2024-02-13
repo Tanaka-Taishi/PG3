5ff02ed0f21696b53b78fefbe70f3b1317e39a8c
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-13 11:58:39 +0900

    02_01

---
+++ Enemy.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include"Enemy.h"
+
+int main(int argc, const char* argv[]) {
+	Enemy enemy;
+	//enemy.Init();
+	enemy.Update();
+	return 0;
+}
---
+++ Enemy.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdio.h>
+
+class Enemy {
+public:
+	void Init();
+
+	void Update();
+
+private:
+
+	enum class Phase {
+		kApproach,
+		kShot,
+		kLeave,
+	};
+
+	void Approach();
+	void Leave();
+	void Shot();
+
+	Phase phase_;
+
+	static void (Enemy::* pPhaseTable[])();
+};
