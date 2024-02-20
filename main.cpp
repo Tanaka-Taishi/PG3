2758f332e2af66feca5d4f16b01bc0121eec4fcd
田中泰申 <k022g0122@g.neec.ac.jp>
2024-02-20 11:24:08 +0900

    02_02

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main() {
 	Box<float, int> b4(1.2f, 2);
 	Box<double, float> b5(4.3, 5.1f);
 	Box<double, int> b6(5.2, 3);
-	/*printf("%d\n%f\n%lf\n", b1, b2, b3);*/
+
 	printf("%d\n", b1.Min());
 	printf("%f\n", b2.Min());
 	printf("%lf\n", b3.Min());
@@ -36,9 +36,3 @@ int main() {
 	printf("%lf\n", b6.Min());
 	return 0;
 }
-
-//#include <stdio.h>
-//
-//int main(int argc, const char* argv[]) {;
-//	return 0;
-//}
